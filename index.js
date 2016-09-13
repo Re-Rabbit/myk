49a4a6a65a0b6a06457a4f579a25a35d620d94cf
yuffiy <yfhj1990@hotmail.com>
2016-09-13 18:16:22 +0800

    update

---
+++ index.js
@@ -0,0 +1,54 @@
+// -*- mode: react -*-
+import Elm from 'react-elm-components'
+import React, { Component } from 'react'
+import $ from 'jquery'
+import { render } from 'react-dom'
+import Snippets from 'Snippets/Main.elm'
+import Slider from 'Slider/index.jsx'
+import SliderPort from 'Slider/port.js'
+
+//import test from './images/photo.jpg'
+
+console.log(test)
+
+class App extends Component {
+  render() {
+	  return (
+	    <div>
+	      <Elm src={Snippets.Main} ports={setupPorts} />
+	      <Slider />
+	    </div>
+	  )
+  }
+}
+
+function setupPorts(ports) {
+  console.log( ports )
+
+  SliderPort(ports)
+
+}
+
+render(
+  <App />,
+  document.getElementById('main')
+)
+
+/**
+   ___browserSync___.socket.emit('test', 2333)
+
+   var ws = new WebSocket('ws://localhost:9999')
+   ws.send('233')
+
+var ws = new WebSocket('ws://localhost:9999')
+___browserSync___.socket.on('LocateError', queries => {
+  ws.send(queries)
+})
+
+
+require.ensure('test2.js', function() {
+
+	let test = require('test2.js')
+	console.log(test)
+})
+*/
